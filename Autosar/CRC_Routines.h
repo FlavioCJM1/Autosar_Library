14bc2b263b93db681557066e99afd93486ab68f2
Flavio <flaviocjm@outlook.es>
2023-07-31 16:20:26 -0600

    Added the CRC library

---
+++ Autosar/CRC_Routines.h
@@ -0,0 +1,9 @@
+#ifndef BFX_8BITS_H__
+#define BFX_8BITS_H__
+
+#include "Platform_Types.h"
+
+uint8 Crc_CalculateCRC8 (const uint8* Crc_DataPtr, uint32 Crc_Length, uint8 Crc_StartValue8, boolean Crc_IsFirstCall);
+
+
+#endif
